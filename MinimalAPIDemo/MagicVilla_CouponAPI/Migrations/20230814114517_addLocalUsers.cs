bc635f55f2d6fe57b40a61ab7e50417e83c08411
muradav <muradav@code.edu.az>
2023-08-14 15:48:52 +0400

    feat: Create Table for Users

---
+++ MinimalAPIDemo/MagicVilla_CouponAPI/Migrations/20230814114517_addLocalUsers.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addLocalUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LocalUsers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocalUsers", x => x.ID);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Coupons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "10OFF");
+
+            migrationBuilder.UpdateData(
+                table: "Coupons",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "20OFF");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LocalUsers");
+
+            migrationBuilder.UpdateData(
+                table: "Coupons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "100FF");
+
+            migrationBuilder.UpdateData(
+                table: "Coupons",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "200FF");
+        }
+    }
+}
